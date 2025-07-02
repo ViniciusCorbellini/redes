0717646bb4d98fc70677d44857151e533217799a
ManoCorbas <corbellinivinicius4@gmail.com>
2025-07-02 18:29:29 -0300

    refactoring NetworkHandler

--- src/main/java/com/mycompany/trabalho_redes/parte1/model/NetworkHandler.java
+++ src/main/java/com/mycompany/trabalho_redes/parte1/model/NetworkHandler.java
@@ -1,13 +1,10 @@
 package com.mycompany.trabalho_redes.parte1.model;
 
 import java.io.IOException;
-import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.InterfaceAddress;
 import java.net.MulticastSocket;
 import java.net.NetworkInterface;
-import java.util.Enumeration;
 import java.util.logging.Logger;
 
 /**
@@ -22,7 +19,6 @@ public class NetworkHandler {
     private MulticastSocket socket;
     private InetAddress group;
     private int port;
-    private NetworkInterface network_inteface;
 
     public NetworkHandler(String multicastIp, int port) throws IOException {
         this.buffer_size = 1024;
@@ -30,40 +26,19 @@ public class NetworkHandler {
         this.port = port;
         
         this.socket = new MulticastSocket(port);
-        this.network_inteface = getValidNetworkInterface();
-        //socket.setNetworkInterface(network_inteface);
         
         join();
-        LOG.info("NetworkHandler criado para grupo " + group.getHostAddress() + ":" + port +
-                 " usando interface: " + network_inteface.getDisplayName()+ "\n");
-    }
-
-    private NetworkInterface getValidNetworkInterface() throws IOException {
-        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-        while (interfaces.hasMoreElements()) {
-            NetworkInterface ni = interfaces.nextElement();
-            if (ni.isUp() && !ni.isLoopback() && !ni.isVirtual()) {
-                for (InterfaceAddress ia : ni.getInterfaceAddresses()) {
-                    if (ia.getAddress() instanceof Inet4Address) {
-                        LOG.info("Interface de rede valida encontrada: " + ni.getDisplayName()+ "\n");
-                        return ni;
-                    }
-                }
-            }
-        }
-        throw new IOException("Nenhuma interface de rede com IPv4 válida encontrada.");
+        LOG.info("NetworkHandler criado para grupo " + group.getHostAddress() + ":" + port +"\n");
     }
 
     public void join() throws IOException {
-        //this.socket.joinGroup(new InetSocketAddress(group, port), network_inteface);
-        this.socket.joinGroup(new InetSocketAddress(group, port), NetworkInterface.getByInetAddress(InetAddress.getLocalHost()));
+        this.socket.joinGroup(group);
         LOG.info("Entrou no grupo multicast: " + group.getHostAddress() + ":" + port+ "\n");
     }
 
     public void leave() throws IOException {
         this.getSocket().leaveGroup(
                 new InetSocketAddress(this.getGroup(), this.getPort()),
-                //network_inteface);
                  NetworkInterface.getByInetAddress(InetAddress.getLocalHost()));
         LOG.info("Saiu do grupo multicast: " + group.getHostAddress() + ":" + port+ "\n");
     }
@@ -102,14 +77,6 @@ public class NetworkHandler {
 
     public void setPort(int port) {
         this.port = port;
-    }
-
-    public NetworkInterface getNetwork_inteface() {
-        return network_inteface;
-    }
-
-    public void setNetwork_inteface(NetworkInterface network_inteface) {
-        this.network_inteface = network_inteface;
     }
 
     @Override
